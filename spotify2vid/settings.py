8ec198c7dd635286dc176a5d61088eeceec0e937
Fakez <drzjamil@gmail.com>
2021-05-14 00:21:52 -0300

    fixed env handler

--- spotify2vid/settings.py
+++ spotify2vid/settings.py
@@ -15,7 +15,7 @@ import os
 import django_heroku
 import dj_database_url
 import decouple
-from .classes import EnvVarsHandler
+from .classes.EnvVarsHandler import EnvVarsHandler
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
