9d401877e6c0cd865ca9d9960545299e47677d31
Fakez <drzjamil@gmail.com>
2021-05-14 02:00:40 -0300

    test heroku

--- spotify2vid/settings.py
+++ spotify2vid/settings.py
@@ -153,6 +153,8 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DISABLE_COLLECTSTATIC=1
+
 
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
