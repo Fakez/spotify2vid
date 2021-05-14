03f429d3b8007768b441603edb92c08a10dc103a
Fakez <drzjamil@gmail.com>
2021-05-14 01:54:46 -0300

    test heroku

--- spotify2vid/settings.py
+++ spotify2vid/settings.py
@@ -19,7 +19,7 @@ from .classes.EnvVarsHandler import EnvVarsHandler
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
-#BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
