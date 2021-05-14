859504f37519148ec2f3a61cb655affcdecc28d1
Fakez <drzjamil@gmail.com>
2021-05-14 10:57:11 -0300

    changed referrer policy again

--- spotify2vid/settings.py
+++ spotify2vid/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'spotify2vid.wsgi.application'
-SECURE_REFERRER_POLICY = 'same-origin'
+SECURE_REFERRER_POLICY = "no-referrer-when-downgrade"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
